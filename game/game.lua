554b593ce252d45f335ce921ea7b3054b7c98ce0
Eliott <eliotts@protonmail.com>
2020-03-26 18:01:21 +0100

    w

--- game/game.lua
+++ game/game.lua
@@ -19,13 +19,14 @@ function init_game()
   state = "title"
 
   init_heart()
-
+  init_lm()
 
 end
 
 function update_game()
-  update_touches()
+
   update_heart()
+  update_lm()
   
 end
 
@@ -34,16 +35,8 @@ function draw_game()
   cls(_p_n(background_clr))
   rctf(0, 0, GW, GH + 50, _p_n(background_clr))
   
-  
   draw_heart()
-  
-  -- for i, t in pairs(touches) do
-    -- add_log(i .. " : " .. t.x .. ", " .. t.y)
-  -- end
-  
-  local cl = click()
-  
-  if cl then add_log(count(touches) .. " : " .. cl.x .. ", " .. cl.y) end
+  draw_lm()
   
   use_font("log")
   print_log()
@@ -56,7 +49,7 @@ function init_controls()
   register_btn("mouse_x", 0, input_id("mouse_position", "x"))
   register_btn("mouse_y", 0, input_id("mouse_position", "y"))
   
-  register_btn("select", 0,  input_id("mouse_button", "lb"))
+  register_btn("c", 0,  input_id("mouse_button", "lb"))
   
 end
 
@@ -64,7 +57,7 @@ function init_fonts()
   
   
   load_font("sugarcoat/TeapotPro.ttf", 16, "log", false)
-  load_font("sugarcoat/TeapotPro.ttf", 32, "32l", false)
+  load_font("sugarcoat/TeapotPro.ttf", 32, "32", false)
   
 end
 
@@ -85,46 +78,101 @@ function init_heart()
   
   heart.clr1 = _p_n("pink")
   heart.clr2 = _p_n("ppink")
+  heart.clr3 = _p_n("white")
 end
 
 function update_heart()
 
-  
-  
+  heart.hover = m_in_rect(heart.x, heart.y, heart.w, heart.h)
+  heart.clicked = btnp("c") and heart.hover 
+  if heart.clicked then lm.count = lm.count + 1 end
 end
 
 function draw_heart()
 
-  rctf(heart.x, heart.y, heart.w, heart.h, heart.clr1)
+  rctf(heart.x, heart.y, heart.w, heart.h, heart_clr())
   
 end
 
 
----------- Touches
+function heart_clr()
 
-function update_touches()
+  if heart.clicked then return heart.clr3
+  elseif heart.hover then return heart.clr2
+  else return heart.clr1 end
+
+end
 
 
-  tch = love.touch.getTouches( )
+
+---------- Love Meter
+
+
+
+
+
+function init_lm()
   
-  touches = {}
+  lm = {}
   
-  for i = 1, count(tch) do
-    xs, ys = love.touch.getPosition( tch[i] )
-    touches[i] = {x = xs, y = ys }
+  lm.length = 8
+  
+  lm.count = 0
+  
+  
+  
+
+end
+
+function update_lm()
+
+end
+
+function draw_lm()
+
+
+  use_font("32")
+  
+  local str = count_to_str(lm.count)
+  
+  -- add_log(lm.count)
+  
+  -- print(lm.count, 0, 0)
+  print(str, 0, 0)
+
+  
+
+end
+
+function count_to_str(count)
+  -- return ("0" * 7) .. count
+  
+  if count > 999999 then return count
+  else
+  
+    local x = 10
+    local n_o_z = 6
+    
+    while count > x - 1 do    
+      x = x * 10
+      n_o_z = n_o_z - 1
+    end
+    
+    return noz(n_o_z) .. count
   end
-  
-  if btn("select") then touches[1] = {x = btnv("mouse_x"), y = btnv("mouse_y") } end
-  
 end
 
-function click()
-
-
-  return btn("select") and {x = btnv("mouse_x"), y = btnv("mouse_y") } or
-        (count(touches) == 1 and touches[1] or nil)
-
+function noz(number)
+  str = ""
+  for i = 1, number do str = str .. "0" end 
+  return str
 end
+
+
+
+
+
+
 
 
 
