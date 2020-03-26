6cde375013a9d37267ad42ae2b01325916f0c86f
Eliott <eliotts@protonmail.com>
2020-03-26 17:16:15 +0100

    touches

--- game/game.lua
+++ game/game.lua
@@ -18,26 +18,28 @@ function init_game()
   spritesheet_grid (16, 16)
   state = "title"
 
+  init_heart()
+
+
 end
 
 function update_game()
-
-
+  update_touches()
+  update_heart()
   
 end
 
 function draw_game()
   
-  
-  -- love.graphics.translate(0.5 * (width - (GW) * zoom),  0.5 * (height - (GH + border/2) * zoom))
-  
-  love.graphics.translate(0, 0)
-  
-  
-  
   cls(_p_n(background_clr))
   rctf(0, 0, GW, GH + 50, _p_n(background_clr))
-  add_log("Android!")
+  
+  
+  draw_heart()
+  
+  for i, t in pairs(touches) do
+    add_log(i .. " : " .. t.x .. ", " .. t.y)
+  end
   
   use_font("log")
   print_log()
@@ -50,6 +52,8 @@ function init_controls()
   register_btn("mouse_x", 0, input_id("mouse_position", "x"))
   register_btn("mouse_y", 0, input_id("mouse_position", "y"))
   
+  register_btn("select", 0,  input_id("mouse_button", "lb"))
+  
 end
 
 function init_fonts()
@@ -59,6 +63,69 @@ function init_fonts()
   load_font("sugarcoat/TeapotPro.ttf", 32, "32l", false)
   
 end
+
+
+
+----------- Heart
+
+
+
+
+function init_heart()
+
+  heart = {}
+  
+  heart.w = 32
+  heart.h = 32
+  
+  heart.x = GW/2 - heart.w/2
+  heart.y = GH/5*2.2 - heart.h/2
+  
+  heart.clr1 = _p_n("pink")
+  heart.clr2 = _p_n("ppink")
+end
+
+function update_heart()
+
+  
+  
+end
+
+function draw_heart()
+
+  rctf(heart.x, heart.y, heart.w, heart.h, heart.clr1)
+  
+end
+
+
+---------- Touches
+
+function update_touches()
+
+
+
+
+
+  tch = love.touch.getTouches( )
+  -- xs = {}
+  -- ys = {}
+  -- for i = 1, #touches do
+    -- ys[i], xs[i] = love.touch.getPosition( touches[i] )
+    -- xs[i] = GW - xs[i]
+  -- end
+  
+  touches = {}
+  
+  for i = 1, count(tch) do
+    xs, ys = love.touch.getPosition( tch[i] )
+    touches[i] = {x = xs, y = ys }
+  end
+  
+  if btn("select") then touches[1] = {x = btnv("mouse_x"), y = btnv("mouse_y") } end
+  
+end
+
+
 
 
 
--- main.lua
+++ main.lua
@@ -7,26 +7,18 @@ sugar.utility.using_package(sugar.S, true)
 
 
 zoom = 2
-local pi = 3.1415
 
 function love.load()
 
-  
-  -- GW = 700 / zoom
-  -- GH = GW * 21/16
-  -- GH = love.graphics.getWidth()/zoom
   GH = love.graphics.getHeight()/zoom
-  -- GH = flr(GH)
-  -- GW = love.graphics.getHeight()/zoom
   GW = love.graphics.getWidth()/zoom
-  -- GW = flr(GW)
 
   init_sugar("? Are you the pizza person ?", GW, GH, zoom )
 
   set_frame_waiting(30)
   
   love.math.setRandomSeed(os.time())
-  -- love.mouse.setVisible(true)
+  love.mouse.setVisible(true)
   
   init_game()
 end
@@ -37,16 +29,7 @@ end
 
 
 function love.draw()
-  
-  -- love.graphics.push()
-  -- love.graphics.translate(GW/2, GH/2)
-  -- love.graphics.rotate(-pi/2) 	
-  -- love.graphics.translate(-GH/2, -GW/2)
-  
   draw_game()
-    
-  -- love.graphics.pop()
-  
 end
 
 
