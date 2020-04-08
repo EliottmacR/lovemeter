516d9a71c5ae40e41c71f534b942c2d53fef9512
Eliott <eliotts@protonmail.com>
2020-04-08 20:02:07 +0200

    t

--- server.lua
+++ server.lua
@@ -138,6 +138,10 @@ function server.update()
                 local count = castle.storage.getGlobal(tostring(v)) or 0          
                 if count then
                   global_count = global_count + count
+                  if v == server_key then
+                    send_clicks = send_clicks + waiting_for_conf
+                    waiting_for_conf = 0
+                  end
                   castle.storage.setGlobal(tostring(v), 0)
                 end
               end
@@ -147,9 +151,6 @@ function server.update()
             castle.storage.setGlobal('global_key', global_count)
             
             castle.storage.setGlobal("alpha", {server_key, get_time()})
-            
-            send_clicks = send_clicks + waiting_for_conf
-            waiting_for_conf = 0         
             
             server_state = "alphaornot"
             doingiamthealpha = false
